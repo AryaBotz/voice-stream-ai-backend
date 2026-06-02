f7b5e88a02532b193be78fdde95bf89ba37f014f
AryaBotz <87101138+AryaBotz@users.noreply.github.com>
2026-06-02 14:08:37 +0700

    Initialize server and WebSocket in index.js

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import { createServer } from "http";
+import { app } from "./app.js";
+import { initWebSocket } from "./websocket/ws.server.js";
+
+const server = createServer(app);
+
+initWebSocket(server);
+
+server.listen(3000, () => {
+  console.log("Server running");
+});

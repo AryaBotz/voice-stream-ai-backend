448dcf7047f945c141a1caaea768fa3fc3e327ee
AryaBotz <87101138+AryaBotz@users.noreply.github.com>
2026-06-02 19:01:09 +0700

    Refactor WebSocket server to use message router
    Replaced audio handling with message routing in WebSocket server.

--- websocket/ws.server.js
+++ websocket/ws.server.js
@@ -1,22 +1,19 @@
 import { WebSocketServer } from "ws";
-import { handleAudio } from "../voice.pipeline.js";
+import { handleMessage } from "./ws.router.js";
 
 export function initWebSocket(server) {
   const wss = new WebSocketServer({ server });
 
   wss.on("connection", (ws) => {
-    ws.on("message", async (msg) => {
-      const data = JSON.parse(msg.toString());
+    console.log("Client connected");
 
-      if (data.type === "audio") {
-        const response = await handleAudio(data.data);
-        ws.send(JSON.stringify({
-          type: "response",
-          text: response
-        }));
-      }
+    ws.on("message", (data) => {
+      handleMessage(ws, data);
     });
 
-    ws.send(JSON.stringify({ type: "connected" }));
+    ws.send(JSON.stringify({
+      type: "ready",
+      text: "server ready"
+    }));
   });
 }
